d2b7109643784b4496e617ef329b612e822b6fca
sleepwalky <keeweery@gmail.com>
2017-04-17 16:27:56 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+
+class App extends Component {
+    render() {
+        return <Button bsStyle="info" bsSize="large">button</Button>;
+    }
+}
+
+export default App;
